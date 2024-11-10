0c4f5816dca4c5111eda3952c00493c71d77ba39
kazkt01 <91418688+kazkt01@users.noreply.github.com>
2024-11-10 17:34:44 +0900

    update

--- functionhen.js
+++ functionhen.js
@@ -48,3 +48,57 @@ document.getElementById("fire").innerHTML = n;
 // document.getElementById("fire").innerHTML = func1(num);
 
 //  #03 処理の流れを確認しよう　←ここが関数の理解の肝
+
+// 自作の関数とIf文の組み合わせ
+
+// let num = Number(prompt("数値を入力してください"));
+// let result;
+// function funcA(num) {
+//   return num * 0.8; // ←掛け算
+// }
+// const resultValue = funcA(num); // ここに格納
+
+// console.log(resultValue);
+// if (resultValue >= 90) {
+//   result = `結果は、${resultValue}で合格です `;
+// } else if (resultValue >= 70) {
+//   result = `結果は、${resultValue}です、頑張りましょう `;
+// } else {
+//   result = `結果は、${resultValue}です、もっと頑張りましょう `;
+// }
+
+// document.getElementById("fire").innerHTML = result;
+
+let num = Number(prompt("number?")); // 入力された値を検知
+let resultA;
+
+function funcA(num) {
+  //promptで入力されたnumを使用し計算する関数
+  return num + 1;
+}
+
+const resultB = funcA(num); //　ここに格納
+
+console.log(resultB); // 確認
+
+if (resultB >= 50) {
+  resultA = `${resultB}で合格です`;
+} else {
+  resultA = `${resultB}です.頑張ってください`;
+}
+
+document.getElementById("fire").innerHTML = resultA;
+
+/*           memo
+> ~より大きい
+> <　~より小さい
+> =　~以上
+> <= ~以下
+> === ~と等しい
+> !=== ~と等しくない
+//////////////////////////
+||　　←　もしくは
+&&←尚且つ
+! は、では、なかったらになる
+
+*/
