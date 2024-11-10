540483346c1481125d25825400fd757d792d326c
kazkt01 <91418688+kazkt01@users.noreply.github.com>
2024-11-10 16:48:17 +0900

    update

--- functionhen.js
+++ functionhen.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // 関数編　　//前12講座
-
+/*
 let a = prompt("何か入力してみてください");
 
 let n; //代入せず宣言だけ
@@ -14,6 +14,37 @@ if (Number(a) <= 70) {
 
 document.getElementById("fire").innerHTML = n;
 
+*/
+
 // ↑遊び
 
+////////////////////////////////////////////////////
+
 //  #01 関数の仕組みを理解しよう  ←明日は、ここから
+// {
+//   // 自作
+//   let e = Number(prompt("入力してください"));
+//   const func1 = () => {
+//     return e + 20;
+//   };
+//   document.getElementById("fire").innerHTML = func1();
+// }
+
+// num ←仮引数
+//letで変数を定義する必要がない←ここ注意点
+// function double(num) {
+//   return num * 2;
+// }
+
+//  #02 関数を作ってみよう
+
+// console.log(double(10)); //実引数
+// console.log(double(4) * 3);
+
+// let num = Number(prompt("number?"));
+// function func1(num) {
+//   return num * 20 + 10;
+// }
+// document.getElementById("fire").innerHTML = func1(num);
+
+//  #03 処理の流れを確認しよう
--- sub.js
+++ sub.js
@@ -1,5 +1,21 @@
 "use strict";
 
-let x = 20;
+// let x = 20;
 
-console.log(x);
+// console.log(x);
+
+// let e = prompt("入力してください");
+// let r;
+
+// if (e <= 90) {
+//   r = `${e}で合格`;
+// } else {
+//   r = `${e}で不合格`;
+// }
+
+// document.getElementById("fire").innerHTML = r;
+// {
+//   for (let e = 1; e <= 10; e++) {
+//     console.log("Hellow");
+//   }
+// }
